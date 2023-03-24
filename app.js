2947a7fb1d7d18ee7d16129b7f22aafd347cac50
Daniel Metz <107957273+D1288@users.noreply.github.com>
2023-03-23 22:26:09 -0700

    disabled input after game is won

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const cellContainer = document.querySelector('.cellContainer')
+const nameInput = document.querySelector('.nameInput')
 let gameBoard = [null, null, null, null, null, null, null, null, null]
 const start = document.querySelector('.startButton')
+const gameContainer = document.querySelector('.gameContainer')
 
+let running = false;
 
 
 function createCells() {
@@ -16,8 +19,7 @@ function createCells() {
 
 
 function startGame() {
-    const nameInput = document.querySelector('.nameInput')
-    const gameContainer = document.querySelector('.gameContainer')
+    running = true
     const displayText = document.querySelector('.displayText')
     const player1 = {
         name: 'Player 1',
@@ -38,12 +40,15 @@ function startGame() {
 
     cellContainer.addEventListener('click', (e) => {
         let cell = e.target
-        if (cell.textContent === '') {
+        if (cell.textContent === '' && running === true) {
             if (currentPlayer === player1) {
                 cell.textContent = currentPlayer.marker
                 gameBoard[Number(cell.classList[1])] = 1
                 currentPlayer = player2
                 displayText.textContent = `It is ${currentPlayer.name}'s turn`
+                // if(displayText.textContent === 'Player 1 Wins'){
+                //     running = false;
+                // }
             } else {
                 cell.textContent = currentPlayer.marker
                 gameBoard[Number(cell.classList[1])] = -1
@@ -65,8 +70,10 @@ function startGame() {
             winningCombos.forEach(combo => {
                 if (gameBoard[combo[0]] + gameBoard[combo[1]] + gameBoard[combo[2]] === 3) {
                     displayText.textContent = 'Player 1 Wins'
+                    running = false
                 } else if (gameBoard[combo[0]] + gameBoard[combo[1]] + gameBoard[combo[2]] === -3) {
                     displayText.textContent = 'Player 2 Wins'
+                    running = false
                 }
             })
         })()
@@ -83,6 +90,7 @@ function reset() {
         cellContainer.textContent = ''
         nameInput.classList.remove('hidden')
         gameContainer.classList.add('hidden')
+        running = false
     })
 }
 
